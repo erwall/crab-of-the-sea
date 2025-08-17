9e6f44bcb18678aff5af7eda6886ca16ca63ae14
Charlie Erwall <charlie.erwall@proton.me>
2025-08-18 00:26:44 +0200

    Update player movement
    Add movement features
    - Jump cancellation
    - Scaled fall gravity
    - Movement acceleration and deceleration
    - Terminal velocity (clamping)

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -2,31 +2,45 @@ class_name PlayableCharacter2D
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const MAX_VELOCITY_WALK = 500.0
+const TERMINAL_VELOCITY = 2000.0
+const JUMP_VELOCITY = 700.0
+const ACCELERATION = 2000.0
+const DECELERATION = 5000.0
+const FALL_GRAVITY_MULTIPLIER = 3.5
 
 
 @onready var sprite: AnimatedSprite2D = $Sprite
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * (FALL_GRAVITY_MULTIPLIER if (velocity.y > 0) else 1.0) * delta
+		velocity = velocity.clampf(-TERMINAL_VELOCITY, TERMINAL_VELOCITY)
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var jumped := Input.is_action_just_pressed("jump") and is_on_floor()
+	var jump_cancelled := Input.is_action_just_released("jump") and velocity.y < 0
+	if jumped:
+		velocity.y = -JUMP_VELOCITY
+	elif jump_cancelled:
+		velocity.y *= 0.5
 
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_horizontal := Input.get_axis("left", "right")
+	var breaking := signf(input_horizontal) + signf(velocity.x) == 0
+	if not input_horizontal:
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION * delta)
+	elif breaking:
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x += ACCELERATION * input_horizontal * delta
+		velocity.x = clampf(velocity.x, -MAX_VELOCITY_WALK, MAX_VELOCITY_WALK)
 
-	if direction:
+	if input_horizontal:
 		sprite.animation = "walk"
 	else:
 		sprite.animation = "idle"
 
-	sprite.flip_h = velocity.x < 0
+	if input_horizontal:
+		sprite.flip_h = input_horizontal < 0
 	sprite.play()
 
 	move_and_slide()
